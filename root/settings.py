f81097bf4ac662991d9608371873867fb5245999
goldendevuz <goldendevuz@gmail.com>
2026-02-08 15:26:50 +0500

    2026.02.08/15:26

--- root/settings.py
+++ root/settings.py
@@ -82,13 +82,9 @@ ASGI_APPLICATION = 'root.wsgi.application'
 WSGI_APPLICATION = 'root.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_PORT', 5432),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
